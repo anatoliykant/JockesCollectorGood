2792b654ee9805e1bc259e264a3e4f82ff351f56
anatoliykant <kant.anatoliy@yandex.ru>
2016-02-10 00:23:53 +0300

    start workTask

--- JockesCollector/MasterViewController.swift
+++ JockesCollector/MasterViewController.swift
@@ -33,6 +33,33 @@ class MasterViewController: RESideMenu {
         not.name //LeftMenuPressed
         not.object //Название конкретного пункта меню
         print(not)
+        
         hideMenuViewController()
+        showJokesForMenuItem(not.object as! String)
+        
+    }
+    
+    func showJokesForMenuItem(name:String){
+        let site: String?
+        let siteName: String?
+        
+        switch(name){
+            case "it-happense":
+            site = "ithappens.me"
+            siteName = "ithappens"
+            
+        case "Bash":
+            site = "bash.im"
+            siteName = "bash"
+        default: return
+        }
+        //создадим в сторибоард центральный вьюконтроллер и приведем его к типу NavigationController 
+        //т.к. мы знаем, что он такого типа
+        let navController = self.storyboard?.instantiateViewControllerWithIdentifier(self.contentViewStoryboardID) as! UINavigationController
+        let jokeViewController = navController.viewControllers.first as! JokesListViewController
+        jokeViewController.site = site!
+        jokeViewController.siteName = siteName!
+        
+        setContentViewController(navController, animated: true)
     }
 }
